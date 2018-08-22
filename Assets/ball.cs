b049d8d34b43643ff0565135a226a7e14cdc45d4
RoroftheCrowd <32059576+RoroftheCrowd@users.noreply.github.com>
2018-08-22 16:10:43 +0100

    Initial commit

---
+++ Assets/ball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "goal")
+        {
+            Destroy(gameObject, .2f);
+            Debug.Log("You Win!!");
+        }
+
+    }
+}
+    
+
+
